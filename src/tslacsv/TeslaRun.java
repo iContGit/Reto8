8075e77600c4b7436b545b38f7b6b89ce871b194
Sebastián Gómez <59988445+iContGit@users.noreply.github.com>
2021-08-20 08:41:18 -0500

    Update TeslaRun.java

--- src/tslacsv/TeslaRun.java
+++ src/tslacsv/TeslaRun.java
@@ -6,9 +6,9 @@ public class TeslaRun {
         String ruta = "C:\\Users\\Sebastian\\Documents\\Flujo de archivos Java\\TSLA.csv"; // cambiar por  ruta personal
         Persistencia a = new Persistencia();
         PersistenciaNio b = new PersistenciaNio();
-        a.punto1(ruta);
-        //punto3 en archivo punto3.txt en GitHub.
+        a.punto1(ruta);       
         b.punto2(ruta);
+         //punto3 en archivo punto3.txt en GitHub.
         b.punto4(ruta);
     }
 }
